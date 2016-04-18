0abc1e7d051de2bc537158af502a162f9a2fd2fb
Marica Odagaki <ento.entotto@gmail.com>
2016-04-18 09:54:03 -0700

    Terraform resources for nginx proxy w/ letsencrypt support

---
+++ terraform/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "host" {
+  ami = "${lookup(var.amis, var.region)}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = [
+    "${aws_vpc.main.default_security_group_id}",
+    "${aws_security_group.allow_ssh.id}",
+    "${aws_security_group.allow_http.id}"
+  ]
+  subnet_id = "${aws_subnet.public.id}"
+  associate_public_ip_address = true
+  user_data = <<EOS
+#!/bin/bash
+echo ECS_CLUSTER=${aws_ecs_cluster.main.name} >> /etc/ecs/ecs.config
+EOS
+  iam_instance_profile = "${aws_iam_instance_profile.ecs_iam_profile.name}"
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_eip" "host" {
+  instance = "${aws_instance.host.id}"
+  vpc = true
+}
+
+output "host_ip" {
+  value = "${aws_eip.host.public_ip}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,52 @@
+variable "name" {
+  description = "String used for naming various resources"
+  default = "tws3"
+}
+variable "region" {
+  default = "us-west-2"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "virtual_host" {}
+variable "letsencrypt_email" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "key_name" {}
+variable "s3_access_key" {}
+variable "s3_secret_key" {}
+variable "s3_bucket_name" {}
+variable "s3_bucket_wiki_path" {}
+variable "basic_auth_user" {}
+variable "basic_auth_pass" {}
+variable "ssh_source_cidr" {
+  description = "CIDR/IP range for SSH"
+  default = "0.0.0.0/0"
+}
+variable "http_source_cidr" {
+  description = "CIDR/IP range for HTTP"
+  default = "0.0.0.0/0"
+}
+#variable "vpc_availability_zones" {
+#  description = "Comma-delimited list of two VPC availability zones in which to create subnets"
+#  default = ""
+#}
+variable "asg_max_size" {
+  description = "Maximum size and initial Desired Capacity of ECS Auto Scaling Group."
+  default = 1
+}
+variable "docker_image" {
+  default = "ento/tiddlywiki-s3fs:0.1.0"
+}
+variable "amis" {
+  default = {
+    us-east-1 = "ami-2b3b6041"
+    us-west-2 = "ami-ac6872cd"
+    eu-west-1 = "ami-03238b70"
+    ap-northeast-1 = "ami-fb2f1295"
+    ap-southeast-2 = "ami-43547120"
+    us-west-1 = "ami-bfe095df"
+    ap-southeast-1 = "ami-c78f43a4"
+    eu-central-1 = "ami-e1e6f88d"
+  }
+}
